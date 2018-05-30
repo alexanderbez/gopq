82122361450edc474c81e281c46404fc803221cc
Aleksandr Bezobchuk <abezobchuk@gmail.com>
2018-05-30 18:02:47 -0400

    Implement initial priority queue

---
+++ priority_queue.go
@@ -0,0 +1,88 @@
+package queue
+
+import "container/heap"
+
+type (
+	// Heapable reflects an interface that an item must implement in order to
+	// be placed into a priority queue. If the priority queue does not require
+	// modifications (i.e. updates or removals), these can be implemented as
+	// no-ops.
+	Heapable interface {
+		Priority(other interface{}) bool
+		Index() int
+		SetIndex(i int)
+	}
+
+	// items is the internal container for a priority queue. It's implementation
+	// should not be exposed publically.
+	items []Heapable
+
+	// PriorityQueue implements a priority queue of Heapable items. Each item
+	// must implement the Heapable interface and is responsible for giving each
+	// item priority respective to every other item. The main benefit of this
+	// implementation is to abstract away any direct heap usage.
+	PriorityQueue struct {
+		queue *items
+	}
+)
+
+// NewPriorityQueue returns a reference to a new initialized PriorityQueue.
+func NewPriorityQueue() *PriorityQueue {
+	pq := &PriorityQueue{queue: &items{}}
+
+	heap.Init(pq.queue)
+	return pq
+}
+
+// Len implements the sort.Interface.
+func (it items) Len() int {
+	return len(it)
+}
+
+// Less implements the sort.Interface.
+func (it items) Less(i, j int) bool {
+	return it[i].Priority(it[j])
+}
+
+// Swap implements the sort.Interface.
+func (it items) Swap(i, j int) {
+	it[i], it[j] = it[j], it[i]
+	it[i].SetIndex(i)
+	it[j].SetIndex(j)
+}
+
+// Push adds a Sortable item to the priority queue.
+func (pq *PriorityQueue) Push(s Heapable) {
+	heap.Push(pq.queue, s)
+}
+
+// Pop removes a Sortable item from the priority queue with the highest
+// priority and returns it.
+func (pq *PriorityQueue) Pop() Heapable {
+	return heap.Pop(pq.queue).(Heapable)
+}
+
+// Size returns the size of the priority queue.
+func (pq *PriorityQueue) Size() int {
+	return pq.queue.Len()
+}
+
+// Push implements the heap interface.
+func (it *items) Push(x interface{}) {
+	n := len(*it)
+	item := x.(Heapable)
+	item.SetIndex(n)
+	*it = append(*it, item)
+}
+
+// Pop implements the heap interface.
+func (it *items) Pop() interface{} {
+	old := *it
+	n := len(old)
+	item := old[n-1]
+	oldIdx := item.Index()
+	item.SetIndex(oldIdx - 1)
+	*it = old[0 : n-1]
+
+	return item
+}
---
+++ priority_queue_test.go
@@ -0,0 +1,53 @@
+package queue
+
+import (
+	"sort"
+	"testing"
+)
+
+type testSortable struct {
+	priority, index int
+}
+
+func (ts *testSortable) Priority(other interface{}) bool {
+	if t, ok := other.(*testSortable); ok {
+		return ts.priority > t.priority
+	}
+
+	return false
+}
+
+func (ts *testSortable) Index() int {
+	return ts.index
+}
+
+func (ts *testSortable) SetIndex(i int) {
+	ts.index = i
+}
+
+func TestPriorityQueue(t *testing.T) {
+	pq := NewPriorityQueue()
+	l := 10
+	e := make([]int, 0, l)
+
+	for i := 0; i < l; i++ {
+		p := (i + 1) * 5
+		e = append(e, p)
+
+		pq.Push(&testSortable{priority: p, index: i})
+	}
+
+	sort.Ints(e)
+
+	for i := l - 1; i >= 0; i-- {
+		r := pq.Pop()
+
+		if r.(*testSortable).priority != e[i] {
+			t.Errorf("incorrect result: expected: %v, got: %v", e[i], r.(*testSortable).priority)
+		}
+	}
+
+	if pq.Size() != 0 {
+		t.Errorf("incorrect result: expected: %v, got: %v", 0, pq.Size())
+	}
+}
