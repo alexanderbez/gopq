be2a9607ffddd9b4f109ad939ed62d9b5b2afb62
Aleksandr Bezobchuk <abezobchuk@gmail.com>
2018-06-04 12:09:49 -0400

    Add error handling unit tests

--- priority_queue_test.go
+++ priority_queue_test.go
@@ -5,59 +5,59 @@ import (
 	"testing"
 )
 
-type testSortable struct {
+type testHeapable struct {
 	priority, index, value int
 }
 
-func (ts *testSortable) Priority(other interface{}) bool {
-	if t, ok := other.(*testSortable); ok {
-		return ts.priority > t.priority
+func (th *testHeapable) Priority(other interface{}) bool {
+	if t, ok := other.(*testHeapable); ok {
+		return th.priority > t.priority
 	}
 
 	return false
 }
 
-func (ts *testSortable) Index() int {
-	return ts.index
+func (th *testHeapable) Index() int {
+	return th.index
 }
 
-func (ts *testSortable) SetIndex(i int) {
-	ts.index = i
+func (th *testHeapable) SetIndex(i int) {
+	th.index = i
 }
 
 func TestPriorityQueue(t *testing.T) {
 	testCases := []struct {
-		set             []*testSortable
+		set             []*testHeapable
 		expectedLength  int
-		expectedResults []*testSortable
+		expectedResults []*testHeapable
 	}{
 		{
-			set:             []*testSortable{},
+			set:             []*testHeapable{},
 			expectedLength:  0,
-			expectedResults: []*testSortable{},
+			expectedResults: []*testHeapable{},
 		},
 		{
-			set: []*testSortable{
-				&testSortable{priority: 1, index: 0, value: 0},
+			set: []*testHeapable{
+				&testHeapable{priority: 1, index: 0, value: 0},
 			},
 			expectedLength: 1,
-			expectedResults: []*testSortable{
-				&testSortable{priority: 1, index: 0, value: 0},
+			expectedResults: []*testHeapable{
+				&testHeapable{priority: 1, index: 0, value: 0},
 			},
 		},
 		{
-			set: []*testSortable{
-				&testSortable{priority: 10, index: 0, value: 0},
-				&testSortable{priority: 8, index: 1, value: 1},
-				&testSortable{priority: 12, index: 2, value: 2},
-				&testSortable{priority: 9, index: 3, value: 3},
+			set: []*testHeapable{
+				&testHeapable{priority: 10, index: 0, value: 0},
+				&testHeapable{priority: 8, index: 1, value: 1},
+				&testHeapable{priority: 12, index: 2, value: 2},
+				&testHeapable{priority: 9, index: 3, value: 3},
 			},
 			expectedLength: 4,
-			expectedResults: []*testSortable{
-				&testSortable{priority: 12, index: 3, value: 2},
-				&testSortable{priority: 10, index: 2, value: 0},
-				&testSortable{priority: 9, index: 1, value: 3},
-				&testSortable{priority: 8, index: 0, value: 1},
+			expectedResults: []*testHeapable{
+				&testHeapable{priority: 12, index: 3, value: 2},
+				&testHeapable{priority: 10, index: 2, value: 0},
+				&testHeapable{priority: 9, index: 1, value: 3},
+				&testHeapable{priority: 8, index: 0, value: 1},
 			},
 		},
 	}
@@ -74,11 +74,15 @@ func TestPriorityQueue(t *testing.T) {
 		}
 
 		j := 0
-		for j < pq.Size() {
-			e := pq.Pop()
+		for pq.Size() != 0 {
+			res, err := pq.Pop()
 
-			if !reflect.DeepEqual(e, tc.expectedResults[j]) {
-				t.Errorf("incorrect result: expected: %v, got: %v", tc.expectedResults[j], e)
+			if err != nil {
+				t.Errorf("incorrect result: unexpected error: %v", err)
+			}
+
+			if !reflect.DeepEqual(res, tc.expectedResults[j]) {
+				t.Errorf("incorrect result: expected: %v, got: %v", tc.expectedResults[j], res)
 			}
 
 			j++
@@ -88,38 +92,38 @@ func TestPriorityQueue(t *testing.T) {
 
 func TestPriorityQueueUpdate(t *testing.T) {
 	testCases := []struct {
-		set             []*testSortable
+		set             []*testHeapable
 		updateIndex     int
 		updatePriority  int
 		expectedLength  int
-		expectedResults []*testSortable
+		expectedResults []*testHeapable
 	}{
 		{
-			set: []*testSortable{
-				&testSortable{priority: 1, index: 0, value: 0},
+			set: []*testHeapable{
+				&testHeapable{priority: 1, index: 0, value: 0},
 			},
 			updateIndex:    0,
 			updatePriority: 10,
 			expectedLength: 1,
-			expectedResults: []*testSortable{
-				&testSortable{priority: 10, index: 0, value: 0},
+			expectedResults: []*testHeapable{
+				&testHeapable{priority: 10, index: 0, value: 0},
 			},
 		},
 		{
-			set: []*testSortable{
-				&testSortable{priority: 10, index: 0, value: 0},
-				&testSortable{priority: 8, index: 1, value: 1},
-				&testSortable{priority: 12, index: 2, value: 2},
-				&testSortable{priority: 9, index: 3, value: 3},
+			set: []*testHeapable{
+				&testHeapable{priority: 10, index: 0, value: 0},
+				&testHeapable{priority: 8, index: 1, value: 1},
+				&testHeapable{priority: 12, index: 2, value: 2},
+				&testHeapable{priority: 9, index: 3, value: 3},
 			},
 			updateIndex:    1,
 			updatePriority: 20,
 			expectedLength: 4,
-			expectedResults: []*testSortable{
-				&testSortable{priority: 20, index: 3, value: 1},
-				&testSortable{priority: 12, index: 2, value: 2},
-				&testSortable{priority: 10, index: 1, value: 0},
-				&testSortable{priority: 9, index: 0, value: 3},
+			expectedResults: []*testHeapable{
+				&testHeapable{priority: 20, index: 3, value: 1},
+				&testHeapable{priority: 12, index: 2, value: 2},
+				&testHeapable{priority: 10, index: 1, value: 0},
+				&testHeapable{priority: 9, index: 0, value: 3},
 			},
 		},
 	}
@@ -132,21 +136,141 @@ func TestPriorityQueueUpdate(t *testing.T) {
 		}
 
 		tc.set[tc.updateIndex].priority = tc.updatePriority
-		pq.Update(tc.set[tc.updateIndex])
+
+		if err := pq.Update(tc.set[tc.updateIndex]); err != nil {
+			t.Errorf("incorrect result: unexpected error: %v", err)
+		}
 
 		if pq.Size() != tc.expectedLength {
 			t.Errorf("incorrect result: expected: %v, got: %v", tc.expectedLength, pq.Size())
 		}
 
 		j := 0
-		for j < pq.Size() {
-			e := pq.Pop()
+		for pq.Size() != 0 {
+			res, err := pq.Pop()
 
-			if !reflect.DeepEqual(e, tc.expectedResults[j]) {
-				t.Errorf("incorrect result: expected: %v, got: %v", tc.expectedResults[j], e)
+			if err != nil {
+				t.Errorf("incorrect result: unexpected error: %v", err)
+			}
+
+			if !reflect.DeepEqual(res, tc.expectedResults[j]) {
+				t.Errorf("incorrect result: expected: %v, got: %v", tc.expectedResults[j], res)
 			}
 
 			j++
 		}
+	}
+}
+
+func TestPriorityQueueRemove(t *testing.T) {
+	testCases := []struct {
+		set             []*testHeapable
+		removeIndex     int
+		expectedLength  int
+		expectedResults []*testHeapable
+	}{
+		{
+			set: []*testHeapable{
+				&testHeapable{priority: 1, index: 0, value: 0},
+			},
+			removeIndex:     0,
+			expectedLength:  0,
+			expectedResults: []*testHeapable{},
+		},
+		{
+			set: []*testHeapable{
+				&testHeapable{priority: 10, index: 0, value: 0},
+				&testHeapable{priority: 8, index: 1, value: 1},
+				&testHeapable{priority: 12, index: 2, value: 2},
+				&testHeapable{priority: 9, index: 3, value: 3},
+			},
+			removeIndex:    2,
+			expectedLength: 3,
+			expectedResults: []*testHeapable{
+				&testHeapable{priority: 10, index: 2, value: 0},
+				&testHeapable{priority: 9, index: 1, value: 3},
+				&testHeapable{priority: 8, index: 0, value: 1},
+			},
+		},
+	}
+
+	for _, tc := range testCases {
+		pq := NewPriorityQueue()
+
+		for _, e := range tc.set {
+			pq.Push(e)
+		}
+
+		if err := pq.Remove(tc.set[tc.removeIndex]); err != nil {
+			t.Errorf("incorrect result: unexpected error: %v", err)
+		}
+
+		if pq.Size() != tc.expectedLength {
+			t.Errorf("incorrect result: expected: %v, got: %v", tc.expectedLength, pq.Size())
+		}
+
+		j := 0
+		for pq.Size() != 0 {
+			res, err := pq.Pop()
+
+			if err != nil {
+				t.Errorf("incorrect result: unexpected error: %v", err)
+			}
+
+			if !reflect.DeepEqual(res, tc.expectedResults[j]) {
+				t.Errorf("incorrect result: expected: %v, got: %v", tc.expectedResults[j], res)
+			}
+
+			j++
+		}
+	}
+}
+
+func TestPriorityQueueErr(t *testing.T) {
+	pq := NewPriorityQueue()
+
+	if _, err := pq.Pop(); err == nil {
+		t.Errorf("incorrect result: expected error")
+	}
+
+	pq.Push(&testHeapable{priority: 1, index: 0, value: 0})
+	pq.Pop()
+
+	if _, err := pq.Pop(); err == nil {
+		t.Errorf("incorrect result: expected error")
+	}
+}
+
+func TestPriorityQueueUpdateErr(t *testing.T) {
+	pq := NewPriorityQueue()
+
+	e1 := &testHeapable{priority: 10, index: 0, value: 0}
+	e2 := &testHeapable{priority: 5, index: 1, value: 1}
+
+	if err := pq.Update(e1); err == nil {
+		t.Errorf("incorrect result: expected error")
+	}
+
+	pq.Push(e1)
+
+	if err := pq.Update(e2); err == nil {
+		t.Errorf("incorrect result: expected error")
+	}
+}
+
+func TestPriorityQueueRemoveErr(t *testing.T) {
+	pq := NewPriorityQueue()
+
+	e1 := &testHeapable{priority: 10, index: 0, value: 0}
+	e2 := &testHeapable{priority: 5, index: 1, value: 1}
+
+	if err := pq.Remove(e1); err == nil {
+		t.Errorf("incorrect result: expected error")
+	}
+
+	pq.Push(e1)
+
+	if err := pq.Remove(e2); err == nil {
+		t.Errorf("incorrect result: expected error")
 	}
 }
